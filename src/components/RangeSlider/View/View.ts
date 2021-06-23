88648f208ce873bfb120b603125128119821a15b
Mikhail <mixail.workmail@yandex.ru>
2021-06-23 16:22:20 +0300

    create RangeSlider elements

--- src/components/RangeSlider/View/View.ts
+++ src/components/RangeSlider/View/View.ts
@@ -7,33 +7,34 @@ export class View {
     this.slider = id ? document.querySelector(id) : null;
   }
 
-  createRangeSlider(settings?: ISettings) {
+  createRangeSlider(settings: ISettings) {
 
-    this.slider!.className = 'range-slider';
+    this.slider!.className = 'range-slider'; // TODO wrap in slider__block || range-slider_block
 
-    if (settings && settings.isTwoRunners === true) {
-      // двойной слайдер
-      let twoRunnersSlider: string = `
-      <div class="range-slider__between">
-        <div class ="range-slider__thumb_from"></div>
-        <div class ="range-slider__thumb_to"></div>
-      </div>
-      `;
-      this.slider?.insertAdjacentHTML('beforeend', twoRunnersSlider);
-      // добавить стили для range-slider__between (длина)
-      // добавить местоположение thumb_from
-      // добавить местоположение thumb_to
-    } else {
-      // одиночный слайдер
-      let oneRunnerSlider: string = `
-      <div class="range-slider__between">
-        <div class ="range-slider__thumb_to"></div>
-      </div>
-      `;
-      this.slider?.insertAdjacentHTML('beforeend', oneRunnerSlider);
-      // добавить стили для range-slider__between (длина)
-      // добавить местоположение thumb_from
-      // добавить местоположение thumb_to
+    // create element range-slider__between
+    const rsBetween: HTMLElement = document.createElement('div');
+    rsBetween.className = 'range-slider__between';
+    // set margin-left и margin-right (from settings)
+    rsBetween.style.marginLeft = settings.thumb_from_value + '%';
+    rsBetween.style.marginRight = 100 - settings.thumb_to_value + '%';
+
+    // if slider with two runners
+    if (settings.isTwoRunners === true) {
+      // create element range-slider__thumb_from
+      const rsThumbFrom: HTMLElement = document.createElement('div');
+      rsThumbFrom.className = 'range-slider__thumb_from';
+      rsBetween.appendChild(rsThumbFrom);
     }
+
+    // create element range-slider__thumb_to
+    const rsThumbTo: HTMLElement = document.createElement('div');
+    rsThumbTo.className = 'range-slider__thumb_to';
+    rsBetween.appendChild(rsThumbTo);
+
+    // add elements to this.slider
+    this.slider?.insertAdjacentElement('beforeend', rsBetween);
+
+    // create scale
+    // add scale to slider 'afterend'
   }
 }
