af74a42dcca485f1c4e3c47504083f6edc789f3e
Mikhail <mixail.workmail@yandex.ru>
2021-07-17 13:43:49 +0300

    fix : change 'TooltipName' to 'ThumbName'

--- src/components/RangeSlider/View/Tooltip.ts
+++ src/components/RangeSlider/View/Tooltip.ts
@@ -1,15 +1,15 @@
-type TooltipName = 'from' | 'to';
+import { ThumbName } from "./Thumb";
 
 export default class Tooltip {
   element: HTMLElement;
 
-  constructor(private name: TooltipName) {
+  constructor(private name: ThumbName) {
     this.element = this.createTooltip();
     this.name = name;
   }
 
   createTooltip(): HTMLElement {
-    const className = `range-slider__tooltip_${this.name}`;
+    const className: string = `range-slider__tooltip_${this.name}`;
 
     const tooltip: HTMLElement = document.createElement('div');
     tooltip.className = className;
