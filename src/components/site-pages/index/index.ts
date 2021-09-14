33e9760b3ce4aed3ce41341a11542772da91fe21
Mikhail <mixail.workmail@yandex.ru>
2021-09-14 05:35:33 +0300

    ref : set values for test

--- src/components/site-pages/index/index.ts
+++ src/components/site-pages/index/index.ts
@@ -6,28 +6,28 @@ import '../../RangeSlider/RangeSlider/RangeSlider';
 
 $('#range-slider').RangeSlider({
   min: 0,
-  max: 1500,
+  max: 1502,
   valueFrom: 1000,
-  valueTo: 1490,
+  valueTo: 1492,
   isScaleVisible: true,
-  step: 10,
+  step: 1,
 });
 
 $('#range-slider2').RangeSlider({
   min: 22,
   max: 150,
-  valueFrom: 35,
-  valueTo: 50,
+  valueTo: 54,
   isTwoRunners: false,
   step: 7,
 });
 
 $('#range-slider3').RangeSlider({
-  min: 10000,
-  max: 15000,
-  valueFrom: 12000,
-  valueTo: 14900,
+  min: 100,
+  max: 550,
+  valueFrom: 120,
+  valueTo: 250,
   isScaleVisible: true,
   isVertical: true,
-  step: 27,
+  isTwoRunners: true,
+  step: 1,
 });
