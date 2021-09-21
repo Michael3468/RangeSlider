5286502a5492c52623385e8a00ef239cb3cb899e
Mikhail <mixail.workmail@yandex.ru>
2021-09-21 11:17:28 +0300

    ref : del undefined type from margin param & add return this

--- src/components/RangeSlider/View/Range.ts
+++ src/components/RangeSlider/View/Range.ts
@@ -9,23 +9,21 @@ export default class Range {
     this.element = createElement('div', 'range-slider__range');
   }
 
-  setMarginFromBegin(margin: number | undefined, isVertical: boolean): void {
-    if (margin === undefined) return;
-
+  setMarginFromBegin(margin: number, isVertical: boolean): Range {
     if (isVertical) {
       this.element.style.marginTop = `${margin}px`;
     } else {
       this.element.style.marginLeft = `${margin}px`;
     }
+    return this;
   }
 
-  setMarginFromEnd(margin: number | undefined, isVertical: boolean): void {
-    if (margin === undefined) return;
-
+  setMarginFromEnd(margin: number, isVertical: boolean): Range {
     if (isVertical) {
       this.element.style.marginBottom = `${margin}px`;
     } else {
       this.element.style.marginRight = `${margin}px`;
     }
+    return this;
   }
 }
