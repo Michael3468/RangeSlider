117fbb93202c4f44148fc4012e27b32316595f5d
Mikhail <mixail.workmail@yandex.ru>
2021-10-06 13:15:01 +0300

    test: add tests for 'removeObserver' method

--- src/components/RangeSlider/Observer/Observer.test.ts
+++ src/components/RangeSlider/Observer/Observer.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-undef */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
@@ -13,6 +14,32 @@ describe('addObserver', () => {
 
     expect(() => {
       observer.addObserver(() => { console.log('test observer'); });
+    }).toThrow();
+  });
+});
+
+describe('removeObserver', () => {
+  const testFn = () => { console.log('test function'); };
+
+  it('should remove observer from observers list', () => {
+    const observer = new Observer();
+
+    observer.addObserver(testFn);
+    expect(observer.observers.length).toBe(1); // должен быть один элемент в массиве
+
+    observer.removeObserver(testFn);
+    expect(observer.observers.length).toBe(0); // должен быть пустой массив
+  });
+
+  it('should throw error', () => {
+    const observer = new Observer();
+
+    observer.addObserver(testFn); // добавляем в массив обзервер
+    expect(observer.observers.length).toBe(1);
+
+    expect(() => {
+      // удаляем из массива обзервер которого там нет
+      observer.removeObserver(() => { console.log('another function for test'); });
     }).toThrow();
   });
 });
