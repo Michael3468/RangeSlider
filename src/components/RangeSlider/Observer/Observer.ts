f90967e5b3076ad6a8e7351371a87937897fc853
Mikhail <mixail.workmail@yandex.ru>
2021-10-07 13:58:20 +0300

    fix : change methods arguments types & change for to forEach

--- src/components/RangeSlider/Observer/Observer.ts
+++ src/components/RangeSlider/Observer/Observer.ts
@@ -1,38 +1,41 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import { ISettings } from '../RangeSlider/types';
+
 export default class Observer {
-  observers: any[];
+  observers: Function[];
 
   constructor() {
     this.observers = [];
   }
 
-  addObserver(fn: object): void {
-    if (typeof fn !== 'function') {
-      throw new Error('observer must be a function');
-    }
-    for (let i = 0; i < this.observers.length; i += 1) {
-      const observer = this.observers[i];
-      if (observer === fn) {
-        throw new Error('observer already in the list');
+  addObserver(fn: Function): void {
+    this.observers.forEach((observer) => {
+      if (observer.toString() === fn.toString()) {
+        throw new Error('Observer is already in the list');
       }
-    }
+    });
     this.observers.push(fn);
   }
 
-  removeObserver(fn: object): void {
-    for (let i = 0; i < this.observers.length; i += 1) {
-      const observer = this.observers[i];
-      if (observer === fn) {
-        this.observers.splice(i, 1);
-        return;
+  removeObserver(fn: Function): void {
+    let isObserverRemoved: boolean = false;
+
+    this.observers.forEach((observer, index) => {
+      if (observer.toString() === fn.toString()) {
+        this.observers.splice(index, 1);
+        isObserverRemoved = true;
       }
+    });
+
+    if (!isObserverRemoved) {
+      throw new Error('Could not find observer in the list of observers');
     }
-    throw new Error('could not find observer in list of observers');
   }
 
-  notifyObservers(data?: any): void {
-    const observersSnapshot: any[] = this.observers.slice(0);
-    for (let i = 0; i < observersSnapshot.length; i += 1) {
-      observersSnapshot[i](data);
-    }
+  notifyObservers(data?: ISettings): void {
+    this.observers.forEach((observer) => {
+      observer(data);
+    });
   }
 }
