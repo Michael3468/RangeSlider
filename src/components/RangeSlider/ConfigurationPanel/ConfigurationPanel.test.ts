9430328d47a481356c61c1d8aaec008a19d0501a
Mikhail <mixail.workmail@yandex.ru>
2021-11-05 16:10:19 +0300

    fix : change event from 'click' to 'change'

--- src/components/RangeSlider/ConfigurationPanel/ConfigurationPanel.test.ts
+++ src/components/RangeSlider/ConfigurationPanel/ConfigurationPanel.test.ts
@@ -20,7 +20,7 @@ beforeEach(() => {
     step: 10,
     valueFrom: 1000,
     valueTo: 1490,
-    isVertical: false,
+    isVertical: true,
     isTwoRunners: true,
     isScaleVisible: true,
     isBarVisible: true,
@@ -52,62 +52,62 @@ describe('private addListeners', () => {
 
     /* number inputs */
     // cpMin listener
-    result.cpMin!.click();
+    result.cpMin!.dispatchEvent(new Event('change'));
     expect(String(result.settings.min)).toBe(result.cpMin!.value);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpMin listener end
 
     // cpMax listener
-    result.cpMax!.click();
+    result.cpMax!.dispatchEvent(new Event('change'));
     expect(String(result.settings.max)).toBe(result.cpMax!.value);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpMax listener end
 
     // cpStep listener
-    result.cpStep!.click();
+    result.cpStep!.dispatchEvent(new Event('change'));
     expect(String(result.settings.step)).toBe(result.cpStep!.value);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpStep listener end
 
     // cpFrom listener
-    result.cpFrom!.click();
+    result.cpFrom!.dispatchEvent(new Event('change'));
     expect(String(result.settings.valueFrom)).toBe(result.cpFrom!.value);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpFrom listener end
 
     // cpTo listener
-    result.cpTo!.click();
+    result.cpTo!.dispatchEvent(new Event('change'));
     expect(String(result.settings.valueTo)).toBe(result.cpTo!.value);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpTo listener end
 
     /* checkbox inputs */
     // cpVertical listener
-    result.cpVertical!.click();
+    result.cpVertical!.dispatchEvent(new Event('change'));
     expect(result.settings.isVertical).toBe(result.cpVertical!.checked);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpVertical listener end
 
     // cpRange listener
-    result.cpRange!.click();
+    result.cpRange!.dispatchEvent(new Event('change'));
     expect(result.settings.isTwoRunners).toBe(result.cpRange!.checked);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpRange listener end
 
     // cpScale listener
-    result.cpScale!.click();
+    result.cpScale!.dispatchEvent(new Event('change'));
     expect(result.settings.isScaleVisible).toBe(result.cpScale!.checked);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpScale listener end
 
     // cpBar listener
-    result.cpBar!.click();
+    result.cpBar!.dispatchEvent(new Event('change'));
     expect(result.settings.isBarVisible).toBe(result.cpBar!.checked);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpBar listener end
 
     // cpTips listener
-    result.cpTips!.click();
+    result.cpTips!.dispatchEvent(new Event('change'));
     expect(result.settings.isTooltipsVisible).toBe(result.cpTips!.checked);
     expect(nObsSpy).toBeCalledWith(settings);
     // cpTips listener end
