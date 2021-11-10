2327d41cd5819b1250d834b8075fd55d11306444
Mikhail <mixail.workmail@yandex.ru>
2021-11-10 07:48:46 +0300

    ref : del import/extensions

--- src/components/RangeSlider/RangeSlider/global.d.ts
+++ src/components/RangeSlider/RangeSlider/global.d.ts
@@ -1,5 +1,7 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable no-unused-vars */
-import { IUserSettings } from './types.d.ts';
+import { IUserSettings } from './types';
 
 export {};
 
