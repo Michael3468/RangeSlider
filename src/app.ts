aab93a01790e18dbce97c30ed989ec4268cd16e0
Mikhail <mixail.workmail@yandex.ru>
2021-11-10 10:11:42 +0300

    ref : del old comments

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,3 @@ import './components/site-pages/index/index';
 import './assets/js/common';
 import './static/favicons';
 import './assets/scss/main.scss';
-
-// //requireAll block
-// function requireAll(requireContext) {
-//   return requireContext.keys().map(requireContext);
-// }
-
-// requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));  // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
-// requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
-// //requireAll block end
