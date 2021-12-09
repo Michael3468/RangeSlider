e2456f32b3d007969efe2ec3dd86ba9bf50831c3
Mikhail <mixail.workmail@yandex.ru>
2021-12-09 17:51:53 +0300

    ref: del favicons from production

--- src/static/favicons.js
+++ src/static/favicons.js
@@ -1,7 +1,9 @@
-const faviconsContext = require.context(
-  '!!file-loader?name=static/favicons/[name].[ext]!.',
-  true,
-  /\.(svg|png|ico|xml|json)$/,
-);
+if (process.env.NODE_ENV !== 'production') {
+  const faviconsContext = require.context(
+    '!!file-loader?name=static/favicons/[name].[ext]!.',
+    true,
+    /\.(svg|png|ico|xml|json)$/,
+  );
 
-faviconsContext.keys().forEach(faviconsContext);
+  faviconsContext.keys().forEach(faviconsContext);
+}
