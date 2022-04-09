a24d509fec1baa8cf0b489d53108f04a0de356a9
Mikhail <mixail.workmail@yandex.ru>
2022-04-09 16:37:50 +0300

    build(eslint): update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,43 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    es6: true,
-    jquery: true,
-    jest: true,
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "es6": true,
+    "jquery": true,
+    "jest": true,
   },
-  extends: [
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     'airbnb-base',
     'plugin:fsd/all',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
   },
-  plugins: [
-    '@typescript-eslint',
-    'fsd',
+  "plugins": [
+    "@typescript-eslint",
+    "fsd",
   ],
-  rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
+  "rules": {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
       {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
       },
     ],
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
-};
+}
