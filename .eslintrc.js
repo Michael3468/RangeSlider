86716c4807d4140c4835431089e3f9e6202e578c
Mikhail <mixail.workmail@yandex.ru>
2022-04-12 07:35:00 +0300

    refactor(eslintrc.js): fix quotes warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,42 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "es6": true,
-    "jquery": true,
-    "jest": true,
+  env: {
+    browser: true,
+    es2021: true,
+    es6: true,
+    jquery: true,
+    jest: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'airbnb-base',
-    'plugin:fsd/all',
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  "plugins": [
-    "@typescript-eslint",
-    "fsd",
+  plugins: [
+    '@typescript-eslint',
   ],
-  "rules": {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
+    'dot-notation': 0,
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "extensions": ['.js', '.jsx', '.ts', '.tsx'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
-}
+};
