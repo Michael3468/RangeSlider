e670e6e986aeeb19432759285b6b93deafd68c1e
Mikhail <mixail.workmail@yandex.ru>
2022-04-13 15:13:50 +0300

    refactor(eslintrc.js): add 'max-classes-per-file': 'off' rule #2
    #2

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,8 @@ module.exports = {
     // note you must disable the base rule as it can report incorrect errors
     'no-empty-function': 'off',
     '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions', 'methods'] }],
+
+    'max-classes-per-file': 'off',
   },
   settings: {
     'import/resolver': {
